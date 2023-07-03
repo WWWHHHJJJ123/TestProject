10d1ad4813ca6a8a4b6cd3b7c5a8c7779b5670d7
WangHaoJie <wanghaojie@ihappy.net.cn>
2023-07-03 15:43:15 +0800

    文件下载

--- SpringBootProject/src/main/java/com/whj/test/filestudy/FileStudyController.java
+++ SpringBootProject/src/main/java/com/whj/test/filestudy/FileStudyController.java
@@ -1,8 +1,12 @@
 package com.whj.test.filestudy;
 
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URLEncoder;
@@ -10,16 +14,18 @@ import java.net.URLEncoder;
 /**
  * @创建人 JieSi
  * @创建时间 2023/6/6
- * @描述  文件上传和下载
+ * @描述 文件上传和下载
  */
 @RestController
+@RequestMapping("/file")
 public class FileStudyController {
 
     // 找一个计算机的磁盘位置，比如 D:\\, 我这里使用的是本项目的路径
-    private static final String BASE_DIR = "E:\\MyLearnProject\\";
+    private static final String BASE_DIR = "E:\\";
 
     /**
      * 文件上传,传完之后在你的 BASE_DIR目录下
+     *
      * @param file
      */
     // 定义接口类型和二级路径，完整的接口url是：/file/upload
@@ -41,30 +47,56 @@ public class FileStudyController {
             e.printStackTrace();
         }
     }
-    // 下载接口，url: /file/download?fileName=xxx
 
+    /**
+     * @Description:下载
+     * @Param downloadPath 文件路径
+     */
     @GetMapping("/download")
-    public void download(@RequestParam String fileName, HttpServletResponse response) {
-        //  新建文件流，从磁盘读取文件流
-        try (FileInputStream fis = new FileInputStream(BASE_DIR + fileName);
-             BufferedInputStream bis = new BufferedInputStream(fis);
-             OutputStream os = response.getOutputStream()) {    //  OutputStream 是文件写出流，讲文件下载到浏览器客户端
-            // 新建字节数组，长度是文件的大小，比如文件 6kb, bis.available() = 1024 * 6
-            byte[] bytes = new byte[bis.available()];
-            // 从文件流读取字节到字节数组中
-            bis.read(bytes);
-            // 重置 response
-            response.reset();
-            // 设置 response 的下载响应头
-            response.setContentType("application/octet-stream");
-            response.setHeader("Content-disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));  // 注意，这里要设置文件名的编码，否则中文的文件名下载后不显示
-            // 写出字节数组到输出流
-            os.write(bytes);
-            // 刷新输出流
-            os.flush();
-        } catch (Exception e) {
-            e.printStackTrace();
+    public String downloadPathFile(@RequestParam("downloadPath") String downloadPath,HttpServletResponse response) {
+        System.out.println("----------------");
+        //设置文件路径
+        File file = new File(downloadPath);
+        //获取文件名称
+        String fileName = file.getName();
+        //判断文件是否存在
+        if (file.exists()) {
+            response.setContentType("application/force-download");// 设置强制下载不打开
+            response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null;
+            BufferedInputStream bis = null;
+            try {
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                OutputStream os = response.getOutputStream();
+                int i = bis.read(buffer);
+                while (i != -1) {
+                    os.write(buffer, 0, i);
+                    i = bis.read(buffer);
+                }
+                file.delete();
+                return "下载成功";
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (bis != null) {
+                    try {
+                        bis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (fis != null) {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
+        return "下载失败";
     }
 
 }
