873da40b5815c7408b780de6d4b066010cd5574b
WangHaoJie <wanghaojie@ihappy.net.cn>
2023-07-29 18:10:36 +0800

    easyexcel的使用

---
+++ StudyThings/src/main/java/com/whj/learn/Test1.java
@@ -0,0 +1,21 @@
+package com.whj.learn;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @Author JieSir
+ * @Date 2023/7/23 14:53
+ * @Version 1.0
+ */
+
+public class Test1 {
+
+    public static final List<String> values=Collections.unmodifiableList(Arrays.asList("PRIVATE_VALUES"));
+    public static void main(String[] args) {
+
+        System.out.println(values.size());
+
+
+    }
+}
